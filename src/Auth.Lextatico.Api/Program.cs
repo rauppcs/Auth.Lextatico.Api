56dba1badc97b51f5a5f6d0ca3b3315a593bcb01
Cassiano S.Raupp <cassiano.raupp@outlook.com>
2022-03-13 22:47:00 +0000

    Revert "Updated Program.cs"

--- src/Auth.Lextatico.Api/Program.cs
+++ src/Auth.Lextatico.Api/Program.cs
@@ -45,11 +45,9 @@ app.UseSwagger();
 
 app.UseSwaggerUI(c => c.SwaggerEndpoint("doc/swagger.json", "Auth Lextatico Api v1"));
 
-app.UseDeveloperExceptionPage();
-
 if (!app.Environment.IsProduction())
 {
-    // app.UseDeveloperExceptionPage();
+    app.UseDeveloperExceptionPage();
 }
 
 if (app.Environment.IsProduction())
