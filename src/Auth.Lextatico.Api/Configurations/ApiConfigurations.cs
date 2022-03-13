da9aa7f7e112c3ecde35a613beba5b102723de3c
Cassiano S.Raupp <cassiano.raupp@outlook.com>
2022-03-13 23:53:43 +0000

    Updated ApiConfigurations.cs

--- src/Auth.Lextatico.Api/Configurations/ApiConfigurations.cs
+++ src/Auth.Lextatico.Api/Configurations/ApiConfigurations.cs
@@ -19,7 +19,7 @@ namespace Auth.Lextatico.Api.Configurations
             services.AddControllers(options =>
             {
                 // FILTERS
-                options.Filters.Add<GlobalExceptionAttribute>();
+                // options.Filters.Add<GlobalExceptionAttribute>();
                 options.Filters.Add<ValidationModelAttribute>();
 
                 // CONVENCTIONS
